4283fcd0b99af2b58581d68799638a4805f65bc9
Seifert Darkside <sirperc@github.com>
2018-03-10 17:02:56 +0100

    Commit con habitación hecha con Quads y primera versión de instanciar muebles. Se crean dentro de la habitación con posición y orientación al azar. El siguiente paso es que no puedan superponerse. Quizás sea mejor buscar unos muebles más pequeños para que haya más juego.

---
+++ ActiuProject/Assets/ActiuProject/Scripts/CreatePrefabs.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePrefabs : MonoBehaviour
+{
+	[SerializeField]
+	Transform[] m_vListOfPrefabs;
+
+	[SerializeField]
+	Transform m_ParentTransform;
+
+	List<Transform> m_lCreatedObjects;
+
+	[SerializeField]
+	float m_fMinX, m_fMaxX, m_fMinZ, m_fMaxZ;
+	int m_iNumberOfObjects = 4;
+
+
+	// Use this for initialization
+	void Start () {
+		Vector3 v3Position;
+		Quaternion qRotation;
+		Transform auxTransform;
+		FurnitureBehavior furnitureBehavior;
+
+		float fMinX, fMaxX, fMinZ, fMaxZ;
+		
+		m_lCreatedObjects = new List<Transform>();
+		for (int i = 0; i < m_iNumberOfObjects; i++)
+		{
+			auxTransform = Transform.Instantiate(m_vListOfPrefabs[i % m_vListOfPrefabs.Length], m_ParentTransform);
+			furnitureBehavior = auxTransform.GetComponent<FurnitureBehavior>();
+			m_lCreatedObjects.Add(auxTransform);
+
+			//Set random rotation
+			qRotation = Quaternion.Euler(0, (float)Random.Range(0, 360), 0);
+
+			auxTransform.SetPositionAndRotation(auxTransform.position, qRotation);
+
+			//Set random position but inside the room
+			fMinX = m_fMinX - furnitureBehavior.GetMinX();
+			Debug.Log(auxTransform.name + ": fMinxX:" + fMinX);
+			fMaxX = m_fMaxX - furnitureBehavior.GetMaxX();
+			Debug.Log(auxTransform.name + ": fManxZ:" + fMinX);
+			fMinZ = m_fMinZ - furnitureBehavior.GetMinZ();
+			Debug.Log(auxTransform.name + ": fMinxX:" + fMinX);
+			fMaxZ = m_fMaxZ - furnitureBehavior.GetMaxZ();
+			Debug.Log(auxTransform.name + ": fManxZ:" + fMinX);
+
+			v3Position = new Vector3(Random.Range(fMinX, fMaxX), 0, Random.Range(fMinZ, fMaxZ));
+				
+			auxTransform.SetPositionAndRotation(v3Position, auxTransform.rotation);
+
+			auxTransform.name = m_vListOfPrefabs[i % m_vListOfPrefabs.Length].name + " " + i / m_vListOfPrefabs.Length;
+		}
+	}
+}
---
+++ ActiuProject/Assets/ActiuProject/Scripts/FurnitureBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureBehavior : MonoBehaviour {
+
+	[SerializeField]
+	Collider m_collider;
+
+	public float GetMinX()
+	{
+		return m_collider.bounds.min.x + transform.position.x;
+	}
+
+	public float GetMinZ()
+	{
+		return m_collider.bounds.min.z + transform.position.z;
+	}
+
+	public float GetMaxX()
+	{
+		return m_collider.bounds.max.x + transform.position.x;
+	}
+
+	public float GetMaxZ()
+	{
+		return m_collider.bounds.max.z + transform.position.z;
+	}
+}
